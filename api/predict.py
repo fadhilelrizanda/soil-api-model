965ff7488be52aaa03a2f39f46b588571abdc5e5
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-10-29 19:23:30 +0700

    update new

--- api/predict.py
+++ api/predict.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, jsonify
-import tensorflow as tf
 import numpy as np
 import joblib
+import tensorflow as tf
 import os
 
 app = Flask(__name__)
 
-# Load your trained model
-model = tf.keras.models.load_model(os.path.join(os.path.dirname(__file__), '../test.h5'))
+# Load the TFLite model
+model_path = os.path.join(os.path.dirname(__file__), '../test.tflite')
+interpreter = tf.lite.Interpreter(model_path=model_path)
+interpreter.allocate_tensors()
 
 # Load the scaler
 scaler = joblib.load(os.path.join(os.path.dirname(__file__), '../scaler.joblib'))
@@ -23,8 +25,18 @@ def predict():
     inputs = np.array([[s1, s2]])
     inputs_scaled = scaler.transform(inputs)
 
-    # Run the prediction with the scaled inputs
-    prediction = model.predict(inputs_scaled)
+    # Get input and output tensor details
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Set the tensor for the input data
+    interpreter.set_tensor(input_details[0]['index'], inputs_scaled.astype(np.float32))
+
+    # Run inference
+    interpreter.invoke()
+
+    # Get the prediction from the output tensor
+    prediction = interpreter.get_tensor(output_details[0]['index'])
     predicted_class = np.argmax(prediction, axis=1)[0]
 
     return jsonify({"prediction": int(predicted_class)})
